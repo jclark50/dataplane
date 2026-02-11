72d2761cafe7c840c5a18646e71173d16d8a7526
jclark50 <jordanjclark50@gmail.com>
2026-02-11 18:24:00 -0500

    final versions of function naming schema

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,6 +2,6 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 mavtime_cpp <- function(x, window) {
-    .Call('_dataplane_mavtime_cpp', PACKAGE = 'dataplane', x, window)
+    .Call(`_dataplane_mavtime_cpp`, x, window)
 }
 
